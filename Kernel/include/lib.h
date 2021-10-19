a9c7447887d16aa834b301dfaa54e7e0664039ac
sol <skonfederak@itba.edu.ar>
2021-10-19 12:00:50 -0300

    agregue strcpy en el .h

--- Kernel/include/lib.h
+++ Kernel/include/lib.h
@@ -7,5 +7,6 @@ void * memset(void * destination, int32_t character, uint64_t length);
 void * memcpy(void * destination, const void * source, uint64_t length);
 extern void _int20();
 char *cpuVendor(char *result);
+void strcpy(char *src, char *dest);
 
 #endif
